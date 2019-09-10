915ecb01aa98fe7056d8fbf671212e6374a33fc2
Weilun Yao <yaoweilun1998@hotmail.com>
2019-09-10 20:11:14 +0800

    search friend

--- lib/views/friends/friend_search_card.dart
+++ lib/views/friends/friend_search_card.dart
@@ -46,39 +46,86 @@ class _FriendSearchCardState extends State<FriendSearchCard>{
     return FutureBuilder<DocumentSnapshot>(
         future: Firestore.instance.collection("users").document(widget.uid).collection('friends').document(widget.user.uid).get(),
         builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          return Card(
-            elevation: 8.0,
-            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(14.0))),
-            margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-            color: Colors.cyan,
-            child: 
-              ListTile(
-
-                contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                leading: Container(
-                  padding: EdgeInsets.only(right: 12.0),
-                  decoration: new BoxDecoration(
-                      border: new Border(
-                          right: new BorderSide(width: 1.0,color: Colors.white,))
-                          ),
-                  child: Icon(Icons.person, color: Colors.white,),
-                  ),
-
-                title: Text("${widget.user.fname} ${widget.user.surname}", style: textStyle,),
-                subtitle: Text("${widget.user.email}", style: cardDue,),
-                trailing: buildTrailing(snapshot),
-            ),
-        );
+          if(snapshot.data == null || !snapshot.data.exists){
+            return buildStranger();
+          }
+          else {
+            return buildFriend();
+          }
       }
     );
   }
 
-  Widget buildTrailing(AsyncSnapshot<DocumentSnapshot> snapshot){
-    if (snapshot.data != null ) {
-      return Icon(Icons.person);
-    }
-    return FlatButton(
-      child: Icon(Icons.add),
+
+  Widget buildStranger(){
+    return Card(
+        elevation: 8.0,
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(14.0))),
+        margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+        color: Colors.cyan,
+        child: 
+          ListTile(
+
+            contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            leading: Container(
+              padding: EdgeInsets.only(right: 12.0),
+              decoration: new BoxDecoration(
+                  border: new Border(
+                      right: new BorderSide(width: 1.0,color: Colors.white,))
+                      ),
+              child: Icon(Icons.person, color: Colors.white,),
+              ),
+
+            title: Text("${widget.user.fname} ${widget.user.surname}", style: textStyle,),
+            subtitle: Text("${widget.user.email}", style: cardDue,),
+            trailing: FlatButton(
+              child: Icon(Icons.add),
+            ),
+        ),
     );
+  }
+
+  Widget buildFriend(){
+    return Card(
+      elevation: 8.0,
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(14.0))),
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      color: Colors.cyan[800],
+      child: 
+        ListTile(
+
+          contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          leading: Container(
+            padding: EdgeInsets.only(right: 12.0),
+            decoration: new BoxDecoration(
+                border: new Border(
+                    right: new BorderSide(width: 1.0,color: Colors.white,))
+                    ),
+            child: Icon(Icons.person, color: Colors.white,),
+            ),
+
+          title: Text("${widget.user.fname} ${widget.user.surname}", style: textStyle,),
+          subtitle: Text("${widget.user.email}", style: cardDue,),
+          
+      ),
+    );
+  }
+
+  void addFriend() async {
+    var user_db = Firestore.instance.collection('users').document(widget.uid).collection('friends');
+    var friend_db = Firestore.instance.collection('users').document(widget.user.uid).collection('friends');
+
+    // set user
+    await user_db.document(widget.user.uid).setData({
+      "fname": widget.user.fname,
+      "surname": widget.user.surname,
+    });
+
+    // set friend
+
+    await user_db.document(widget.user.uid).setData({
+      "fname": widget.user.fname,
+      "surname": widget.user.surname,
+    });
   }
 }
